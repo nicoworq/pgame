cf22cce3faa37d62277b0c0a280162c8c4f10b88
nicoworq <nicolas@worq.com.ar>
2015-12-15 12:20:10 -0300

    .

--- back/proyecto_beta.php
+++ back/proyecto_beta.php
@@ -14,7 +14,7 @@ $participantes = $pg->getRankingBackend();
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Pascal Navidad</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
