605227239ee6e4cfba74f712cd59bca0cda4609e
Nico <nicolas@worq.com.ar>
2019-06-12 15:20:48 -0300

    cambios de año

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ include_once './php/TokenCSRF.php';
 include_once './php/Pascal.php';
 
 $tk = new TokenCSRF();
-
 $pg = new Pascal();
 ?>
 <!doctype html>
@@ -14,7 +13,7 @@ $pg = new Pascal();
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Navidad en Pascal</title>
+        <title>Navidad en Pascal 2019</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- php/connection.php
+++ php/connection.php
@@ -4,3 +4,10 @@ $hostname = 'localhost';
 $username = 'root';
 $password = '';
 $dbName = 'pascal_game';
+/*
+$hostname = 'localhost';
+$username = 'dbu_pascal_nav';
+$password = 'drUjavA3';
+$dbName = 'dbs_pascal_nav';
+ *
+ */
